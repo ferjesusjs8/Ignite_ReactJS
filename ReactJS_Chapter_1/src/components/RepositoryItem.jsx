93e156d6d71f7044c3b9c39dcbd9c467e7c3cc67
fernando.js8 <fernando.js8@outlook.com>
2022-10-20 20:00:30 -0300

    useState react hook class

---
+++ ReactJS_Chapter_1/src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props){
+    return (
+        <li>
+            <strong>{props.repository?.name ?? 'default'}</strong>
+            <p>{props.repository?.description ?? 'default'}</p>
+
+            <a href={props.repository?.link ?? 'default'}>
+                Acessar Repositorio
+            </a>
+        </li>);
+}
